2df3db985847c06489fc92bf3523a4e1788f7740
kellytbn <kellytbn14@gmail.com>
2021-11-06 17:18:51 -0500

    Configuraciones iniciales

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from '../config/configuration';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      validationOptions: {
+        allowUnknown: false,
+        abortEarly: true,
+      },
+    }),
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: configuration().database.host,
+      port: configuration().database.port,
+      username: configuration().database.username,
+      password: configuration().database.password,
+      database: configuration().database.dbname,
+      entities: [
+
+      ],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    Logger
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { initializeTransactionalContext } from 'typeorm-transactional-cls-hooked';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  //swagger configurations
+  console.log(process.env.APP_ENV);
+  if (process.env.APP_ENV !== 'pdn') {
+    const options = new DocumentBuilder()
+      .setTitle('Daily book API')
+      .setVersion('1.0')
+      .addSecurity('basic', {
+        type: 'http',
+        scheme: 'basic',
+      })
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  // Global Middlewares
+  app.use(await compression());
+  app.use(await helmet());
+  app.enableCors();
+
+  initializeTransactionalContext();
+
+  // start up server
+  await app.listen(configService.get('PORT', 3000));
 }
 bootstrap();
