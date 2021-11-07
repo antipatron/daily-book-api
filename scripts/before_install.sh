31f31e06d4c427c8a83997aac34b475eedd0e89f
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-11-07 14:42:31 -0500

    Se actualiza build.sh para el deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,8 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install v14.8.1
+nvm use v14.18.1
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/node-js/daily-book-api"
@@ -11,5 +12,5 @@ if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+  mkdir -p ${DIR}
 fi
