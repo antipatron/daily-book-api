319123b7c199e9e7306fe4f9e9754aa1a3460578
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-11-21 15:41:37 -0500

    Se crear metodo para crear proveedores

--- src/modules/controller/provider.controller.ts
+++ src/modules/controller/provider.controller.ts
@@ -1,16 +1,43 @@
-import { Controller, Get, HttpStatus, Query } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, Post, Query } from "@nestjs/common";
 import { ProviderFacadeService } from '../facade/provider.facade.service';
 import { StandardResponse } from "../../utils/http-response/standard-response";
+import { ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ProductDto } from "../dto/product.dto";
+import { ProductDetailDto } from "../dto/product-detail.dto";
+import { MESSAGES_RESPONSE } from "../../utils/enums/messages-response.enum";
+import { ProviderDto } from "../dto/provider.dto";
 
+@ApiTags('Providers')
 @Controller('api/provider')
 export class ProviderController {
   constructor(private readonly facade: ProviderFacadeService) {}
 
   @Get('/filter')
+  @ApiResponse({ status: 200, description: 'Successful.'})
+  @ApiResponse({ status: 403, description: 'Forbidden.'})
+  @ApiBody({
+    description: 'List of providers',
+    type: ProductDto,
+  })
   public async findFiltered(@Query() query): Promise<StandardResponse<any[]>> {
     return {
       status: HttpStatus.OK,
       body: await this.facade.findProvidersFilter(query['seller'], query['name'], query['company'])
+    };
+  }
+
+  @Post()
+  @ApiResponse({ status: 200, description: 'Successful.'})
+  @ApiResponse({ status: 403, description: 'Forbidden.'})
+  @ApiBody({
+    description: 'Product created',
+    type: ProductDetailDto,
+  })
+  public async saveProductsFull(@Body() providerDto: ProviderDto): Promise<StandardResponse<any>> {
+    return {
+      status: HttpStatus.OK,
+      message: MESSAGES_RESPONSE.CREATED,
+      body: await this.facade.saveProvider(providerDto)
     };
   }
 
--- src/modules/facade/provider.facade.service.ts
+++ src/modules/facade/provider.facade.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { ProviderService } from '../service/provider.service';
+import { Injectable } from "@nestjs/common";
+import { ProviderService } from "../service/provider.service";
 import { RequestErrorException } from "../../utils/exception/request-error.exception";
 import { MESSAGES_EXCEPTION } from "../../utils/exception/messages-exception.enum";
+import { Transactional } from "typeorm-transactional-cls-hooked";
+import { ProviderDto } from "../dto/provider.dto";
+import { ProviderEntity } from "../entity/provider.entity";
 
 @Injectable()
 export class ProviderFacadeService {
@@ -12,6 +15,33 @@ export class ProviderFacadeService {
       throw new RequestErrorException(MESSAGES_EXCEPTION.REQUEST_CLIENT_EXCEPTION);
     }
     return await this.providerService.findProvidersFilter(seller, name, company)
+  }
+
+  @Transactional()
+  public async saveProvider(providerDto: ProviderDto): Promise<ProviderDto> {
+    let providerSearch;
+    if(providerDto.identifier){
+       providerSearch= await this.providerService.findByIdentifier(providerDto.identifier)
+    }
+
+    if(providerSearch){
+      throw new RequestErrorException(MESSAGES_EXCEPTION.DATA_DUPLICATE_EXCEPTION);
+    }
+
+    let providerEntity = new ProviderEntity();
+    providerEntity.identifier=providerDto.identifier;
+    providerEntity.providerName=providerDto.providerName
+    providerEntity.sellerName=providerDto.sellerName
+    providerEntity.address=providerDto.address
+    providerEntity.email=providerDto.email
+    providerEntity.phone1=providerDto.phone1
+    providerEntity.phone2=providerDto.phone2
+    providerEntity.phone3=providerDto.phone3
+    providerEntity.companyId=providerDto.companyId
+
+    let providerSaved = await this.providerService.save(providerEntity)
+    return providerSaved;
+
   }
 
 }
