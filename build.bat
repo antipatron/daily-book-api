52f39beef030e4761c7273d2e956314d338440b8
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-12-06 13:01:07 -0500

    se actualiza bat

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@ rmdir C:\Users\juang\OneDrive\Documents\prueba-jenkins\pipeline-qa-daily /s /q
 cd ..
 xcopy pipeline-qa-daily3 C:\Users\juang\OneDrive\Documents\prueba-jenkins\pipeline-qa-daily /e /i
 cd C:\Users\juang\OneDrive\Documents\prueba-jenkins\pipeline-qa-daily
+npm install
 nest start
