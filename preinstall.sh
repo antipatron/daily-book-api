78b4c129d303e44a1ff787d451a7e486ba0956de
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-12-07 17:32:25 -0500

    se actualiza para nest

--- preinstall.sh
+++ preinstall.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 pwd
 node -v
 
 npm install
 npm run build
 # pm2 start dist/src/main.js --name daily-book-api
-npm run start:dev > app.out.log 2> app.err.log < /dev/null &
+nest start --watch > app.out.log 2> app.err.log < /dev/null &
