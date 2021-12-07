a6edd28137ae825d8c83a62f1a8e2071c05b0e06
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-12-07 17:36:16 -0500

    dev

--- preinstall.sh
+++ preinstall.sh
@@ -5,4 +5,4 @@ node -v
 npm install
 npm run build
 # pm2 start dist/src/main.js --name daily-book-api
-nest start --watch > app.out.log 2> app.err.log < /dev/null &
+npm run start:dev > app.out.log 2> app.err.log < /dev/null &
